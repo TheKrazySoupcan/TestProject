36f7dafd30d817f5502993de588a285bc35e2c57
TheKrazySoupcan <thekrazysoupcan@gmail.com>
2017-05-04 14:36:13 +1000

    Uploading Files

---
+++ Assets/cannon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour {
+    public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hitRef;
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position,transform.forward,out hitRef))
+            {
+            hitRef.transform.GetComponent<Rigidbody>().AddForce(Vector3.up * 1000);
+            
+        }
+	}
+}
